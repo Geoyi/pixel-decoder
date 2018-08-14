40ea3b4edf4e3479b0bbf3063c4f75980acda7e4
Zhuangfang Yi <geospatialanalystyi@gmail.com>
2018-08-14 14:18:09 -0400

    add setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from imp import load_source
 from os import path
 import io
 
-__version__ = load_source('PACKAGENAME.version', 'PACKAGENAME/version.py').__version__
+__version__ = load_source('pixel_decoder.version', 'pixel_decoder/version.py').__version__
 
 here = path.abspath(path.dirname(__file__))
 
@@ -15,27 +15,30 @@ with io.open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
 install_requires = [x.strip() for x in all_reqs if 'git+' not in x]
 dependency_links = [x.strip().replace('git+', '') for x in all_reqs if 'git+' not in x]
 
+#readme
+with open('README.md') as f:
+    readme = f.read()
+    
 setup(
-    name='PACKAGENAME',
-    author='',
-    author_email='',
+    name='pixel_decoder',
+    author='Zhuangfang NaNa Yi',
+    author_email='nana@developmentseed.org',
     version=__version__,
-    description='python-seed',
-    url='https://github.com/',
+    description='A deep learning algorithm collection to train satellite segmentation',
+    url='https://github.com/Geoyi/pixel-decoder',
     license='MIT',
     classifiers=[
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
     ],
     keywords='',
     entry_points={
-        'console_scripts': ['PACKAGENAME=PACKAGENAME.main:cli'],
+        'console_scripts': ['pixel_decoder=pixel_decoder.main:cli'],
     },
     packages=find_packages(exclude=['docs', 'tests*']),
     include_package_data=True,
     install_requires=install_requires,
     dependency_links=dependency_links,
+    long_description=readme,
+    long_description_content_type="text/markdown"
 )
